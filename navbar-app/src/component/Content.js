82dc1c0f57dbd7d45925adc2bccb3e8fdfeb1213
huynt195 <trunghuy12t@gmail.com>
2022-01-05 16:59:56 +0700

    add content

--- navbar-app/src/component/Content.js
+++ navbar-app/src/component/Content.js
@@ -2,7 +2,9 @@ import React from 'react';
 const Content = () => {
     return (
         <div className="main-content">
-            abc     
+            <div className="content">
+            This example project shows independently built and deployed microfrontends that use React and single-spa. Each nav link above takes you to a different microfrontend.    
+            </div> 
         </div>
     )
 }
