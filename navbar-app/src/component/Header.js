790a05eede1c998f9e9af25deb2ac0c7bb4b4f4e
huynt195 <trunghuy12t@gmail.com>
2022-01-05 16:38:01 +0700

    add navbar app

---
+++ navbar-app/src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div className='d-flex header'>
+            <div className='header-link'>People</div>
+            <div className='header-link'>Planets</div>
+            <div className='ml-auto header-link'>Github project</div>
+        </div>
+    )
+}
+export default Header
---
+++ navbar-app/src/root.component.js
@@ -0,0 +1,16 @@
+
+import {
+  Header,
+  Content
+} from './component';
+import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Root = (props) => {
+  return <div>
+    <Header />
+    <Content />
+  </div>
+}
+
+export default Root;
