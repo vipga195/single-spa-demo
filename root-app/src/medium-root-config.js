1e019c922c6d6bbbdcf60d17d818b96ddfbf409b
huynt195 <trunghuy12t@gmail.com>
2022-01-05 16:33:31 +0700

    add root app

---
+++ root-app/src/medium-root-config.js
@@ -0,0 +1,16 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@medium/navbar",
+  app: () => System.import("@medium/navbar"),
+  activeWhen: ["/"],
+});
+// registerApplication({
+//   name: "@medium/navbar",
+//   app: () => System.import("@medium/navbar"),
+//   activeWhen: ["/"]
+// });
+
+start({
+  urlRerouteOnly: true,
+});
